4d270d7f538f172309e41381e657c3d7d1d42dc6
Muhammad Zeeshan Tassawar <88225411+rajazeeshan209@users.noreply.github.com>
2021-11-25 22:00:28 +0500

    BottomNav with FloatingActionButton

--- bottom-nav.dart
+++ bottom-nav.dart
@@ -1,5 +1,10 @@
 // ignore_for_file: file_names
 
+import 'package:ecommerce/screens/cart.dart';
+import 'package:ecommerce/screens/feeds.dart';
+import 'package:ecommerce/screens/home.dart';
+import 'package:ecommerce/screens/search.dart';
+import 'package:ecommerce/screens/user_info.dart';
 import 'package:flutter/material.dart';
 
 class BottomNav extends StatefulWidget {
@@ -26,24 +31,59 @@ class _BottomNavState extends State<BottomNav> {
       body: PageView(
         controller: pageController,
         children: [
-          Container(color: Colors.red),
-          Container(color: Colors.blue),
-          Container(color: Colors.cyan),
-          Container(color: Colors.grey),
-          Container(color: Colors.green),
+          Container(child: Home()),
+          Cart(),
+          Search(),
+          Feeds(),
+          UserInfo()
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem> [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
-        BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'Feed'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-        BottomNavigationBarItem(icon: Icon(Icons.info), label: 'User Info')
-      ],
-      currentIndex: _SelectedIndex,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey,
-      onTap: onTapped),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 0.01,
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+          height: kBottomNavigationBarHeight *0.98,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                top: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5
+                )
+              )
+            ),
+          child: BottomNavigationBar(
+            onTap: onTapped,
+            backgroundColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Theme.of(context).textSelectionColor,
+            selectedItemColor: Colors.purple,
+            currentIndex: _SelectedIndex,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+              BottomNavigationBarItem(icon: Icon(null), label: 'Search'),
+              BottomNavigationBarItem(icon: Icon(Icons.rss_feed), label: 'Feed'),
+              BottomNavigationBarItem(icon: Icon(Icons.info), label: 'User Info')
+            ],
+          ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: 
+        FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: Padding(padding: const EdgeInsets.all(8.0),
+      child: FloatingActionButton(
+        hoverElevation: 10,
+        splashColor: Colors.grey,
+        tooltip: 'Search',
+        elevation: 4,
+        child: const Icon(Icons.search),
+        onPressed: () => setState((){
+          _SelectedIndex = 2;
+        }),
+      )),
     );
   }
 }
